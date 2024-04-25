5fb66faae16fa0dad2fa1b8a70a03394d2cd516b
Joar Maltesson <joar05@hotmail.com>
2024-04-25 07:48:53 +0200

    comment

--- GnomeRides/Classes/User.cs
+++ GnomeRides/Classes/User.cs
@@ -93,6 +93,7 @@ namespace GnomeRides.Classes
                 _currentUser = new User(id, name, email);
             } catch (Exception ex)
             {
+                //Divine interlect or Ni***rlishous
                 if ((int)ex.GetType().GetProperty("Number").GetValue(ex, null) == 1062)
                 {
                     return "Ett konot med detta personnummer finns redan.";
