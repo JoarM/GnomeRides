dd07b5539b13f17561e3a8432c8ee3498a039e3b
Joar Maltesson <joar05@hotmail.com>
2024-05-07 15:17:07 +0200

    start booking

--- GnomeRides/View/CarPage.xaml.cs
+++ GnomeRides/View/CarPage.xaml.cs
@@ -22,13 +22,34 @@ namespace GnomeRides.View
     /// </summary>
     public partial class CarPage : Page
     {
+        private Car car;
         public CarPage(string reg_nr)
         {
             InitializeComponent();
             (Car?, string?) res = CarController.GetCarByRegNr(reg_nr);
             Car? car = res.Item1;
             string? error = res.Item2;
-            TxtBlkModel.Text = $"{car.Manufacturer} {car.Model}";
+            this.car = car;
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri("https://unifleet.se/wp-content/uploads/2020/10/Volvo-V60-Recharge-Vapour-Grey.png");
+            bitmap.EndInit();
+
+            ImgCar.Source = bitmap;
+        }
+
+        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TxtBlkPrice.Text = $"Pris: {Calendar.SelectedDates.Count * car.DailyRate / 100} kr";
+        }
+
+        private void BtnBook_Click(object sender, RoutedEventArgs e)
+        {
+            string? error = car.LoanCar(DateOnly.FromDateTime(Calendar.SelectedDates.First()), DateOnly.FromDateTime(Calendar.SelectedDates.Last()));
+            if (error != null)
+            {
+
+            }
         }
     }
 }
