48b078ddcc18242d17f17ca4e8bb618584798599
SIhan_Li <5ih4n.san@gmail.com>
2024-05-11 16:32:28 +0200

    Sexigg van och mc update

--- GnomeRides/Components/VanCard.xaml.cs
+++ GnomeRides/Components/VanCard.xaml.cs
@@ -1,6 +1,19 @@
 ﻿using GnomeRides.Classes;
+using GnomeRides.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace GnomeRides.Components
 {
@@ -9,6 +22,7 @@ namespace GnomeRides.Components
     /// </summary>
     public partial class VanCard : UserControl
     {
+        private Van van;
         public VanCard(Van van)
         {
             InitializeComponent();
@@ -17,9 +31,16 @@ namespace GnomeRides.Components
             bitmap.UriSource = new Uri("https://unifleet.se/wp-content/uploads/2020/10/Volvo-V60-Recharge-Vapour-Grey.png");
             bitmap.EndInit();
 
+            this.van = van;
             ImgCover.Source = bitmap;
             TxtBlkName.Text = $"{van.Manufacturer} {van.Model}";
             TxtBlkPrice.Text = $"{van.DailyRate / 100} kr/dag";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService svc = NavigationService.GetNavigationService(this);
+            svc.Navigate(new VanPage(van.RegNr));
         }
     }
 }
--- GnomeRides/View/VanPage.xaml.cs
+++ GnomeRides/View/VanPage.xaml.cs
@@ -18,79 +18,80 @@ using System.Windows.Shapes;
 namespace GnomeRides.View
 {
     /// <summary>
-    /// Interaction logic for MotorcyclePage.xaml
+    /// Interaction logic for VanPage.xaml
     /// </summary>
-    public partial class MotorcyclePage : Page
+    public partial class VanPage : Page
     {
-        private readonly Motorcycle? motorcycle;
-        public MotorcyclePage(string reg_nr)
+        private readonly Van? van;
+        public VanPage(string reg_nr)
         {
             InitializeComponent();
-            (Motorcycle?, string?) res = MotorcycleController.GetMotorcycleByRegNr(reg_nr);
-            Motorcycle? motorcycle = res.Item1;
+            (Van?, string?) res = VanController.GetVanByRegNr(reg_nr);
+            Van? van = res.Item1;
             string? error = res.Item2;
 
-            this.motorcycle = motorcycle;
+            this.van = van;
             BitmapImage bitmap = new BitmapImage();
             bitmap.BeginInit();
             bitmap.UriSource = new Uri("https://unifleet.se/wp-content/uploads/2020/10/Volvo-V60-Recharge-Vapour-Grey.png");
             bitmap.EndInit();
 
-            ImgMotorcycle.Source = bitmap;
+            ImgVan.Source = bitmap;
             this.Loaded += (s, e) =>
             {
-                if (motorcycle == null)
+                if (van == null)
                 {
                     NavigationService.Navigate(new Error(error));
                 }
             };
-            if (motorcycle != null)
+            if (van != null)
             {
-                TxtBlkModel.Text = $"{motorcycle.Manufacturer} {motorcycle.Model}";
+                TxtBlkModel.Text = $"{van.Manufacturer} {van.Model}";
                 InfoGrid.AddChild(new TextBlock()
                 {
-                    Text = $"Säten: {motorcycle.Seats}",
+                    Text = $"Säten: {van.Seats}",
                     FontWeight = FontWeights.Medium,
                 });
                 InfoGrid.AddChild(new TextBlock()
                 {
-                    Text = $"Miltal: {motorcycle.Mileage} mil",
+                    Text = $"Miltal: {  van.Mileage} mil",
                     FontWeight = FontWeights.Medium,
                 });
                 InfoGrid.AddChild(new TextBlock()
                 {
-                    Text = $"Hjul: {motorcycle.Wheels}",
+                    Text = $"Hjul: {van.Wheels}",
                     FontWeight = FontWeights.Medium,
                 });
                 InfoGrid.AddChild(new TextBlock()
                 {
-                    Text = $"Drivmedel: {motorcycle.FuelType}",
+                    Text = $"Drivmedel: {van.FuelType}",
                     FontWeight = FontWeights.Medium,
                 });
                 InfoGrid.AddChild(new TextBlock()
                 {
-                    Text = $"Cylinder: {motorcycle.CC} CC",
+                    Text = $"MaxVikt: { van.MaxWeight} CC",
                     FontWeight = FontWeights.Medium,
                 });
             }
+
         }
 
         private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (motorcycle == null)
+            if (van == null)
             {
                 return;
             }
-            TxtBlkPrice.Text = $"Pris: {Calendar.SelectedDates.Count * motorcycle.DailyRate / 100} kr";
+            TxtBlkPrice.Text = $"Pris: {Calendar.SelectedDates.Count * van.DailyRate / 100} kr";
         }
 
         private void BtnBook_Click(object sender, RoutedEventArgs e)
         {
-            if (motorcycle == null)
+            if (van == null)
             {
                 return;
             }
-            string? error = motorcycle.LoanMotorcycle(DateOnly.FromDateTime(Calendar.SelectedDates.First() > Calendar.SelectedDates.Last() ? Calendar.SelectedDates.Last() : Calendar.SelectedDates.First()), DateOnly.FromDateTime(Calendar.SelectedDates.First() < Calendar.SelectedDates.Last() ? Calendar.SelectedDates.Last() : Calendar.SelectedDates.First()));
+            string? error = van.LoanVan(DateOnly.FromDateTime(Calendar.SelectedDates.First() > Calendar.SelectedDates.Last() ? Calendar.SelectedDates.Last() : Calendar.SelectedDates.First()), DateOnly.FromDateTime(Calendar.SelectedDates.First() < Calendar.SelectedDates.Last() ? Calendar.SelectedDates.Last() : Calendar.SelectedDates.First()));
             if (error != null)
             {
 
